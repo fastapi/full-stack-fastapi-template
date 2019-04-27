ce2f673dea85d033bb265e9462a9b90ea9fbc29d
ebreton <email>
2019-04-27 20:36:17 +0200

    Add normaluser_token_headers fixture for tests

--- {{cookiecutter.project_slug}}/backend/app/app/tests/conftest.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
+from app.core import config
 from app.tests.utils.utils import get_server_api, get_superuser_token_headers
+from app.tests.utils.user import byemail_authentication_token
 
 
 @pytest.fixture(scope="module")
@@ -11,3 +13,8 @@ def server_api():
 @pytest.fixture(scope="module")
 def superuser_token_headers():
     return get_superuser_token_headers()
+
+
+@pytest.fixture(scope="module")
+def normaluser_token_headers():
+    return byemail_authentication_token(config.EMAIL_TEST_USER)
