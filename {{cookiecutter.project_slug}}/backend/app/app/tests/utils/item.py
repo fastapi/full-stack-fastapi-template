b792e0d1c46f7ac7dfab16106fe790ad3c3000c6
ebreton <email>
2019-05-20 21:57:58 +0200

    Use Faker to create random objects

--- {{cookiecutter.project_slug}}/backend/app/app/tests/utils/item.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/utils/item.py
@@ -1,16 +1,19 @@
 from app import crud
+from app.core import config
 from app.db.session import db_session
 from app.models.item import ItemCreate
 from app.tests.utils.user import create_random_user
-from app.tests.utils.utils import random_lower_string
+from faker import Faker
+
+fake = Faker(config.LOCALE_FOR_TESTS)
 
 
 def create_random_item(owner_id: int = None):
     if owner_id is None:
         user = create_random_user()
         owner_id = user.id
-    title = random_lower_string()
-    description = random_lower_string()
+    title = fake.sentence(nb_words=6, variable_nb_words=True)
+    description = ' '.join(fake.paragraphs(nb=3))
     item_in = ItemCreate(title=title, description=description, id=id)
     return crud.item.create(
         db_session=db_session, item_in=item_in, owner_id=owner_id
--- {{cookiecutter.project_slug}}/backend/app/app/tests/utils/user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/utils/user.py
@@ -4,7 +4,9 @@ from app import crud
 from app.core import config
 from app.db.session import db_session
 from app.models.user import UserCreate
-from app.tests.utils.utils import random_lower_string
+from faker import Faker
+
+fake = Faker(config.LOCALE_FOR_TESTS)
 
 
 def user_authentication_headers(server_api, email, password):
@@ -18,8 +20,19 @@ def user_authentication_headers(server_api, email, password):
 
 
 def create_random_user():
-    email = random_lower_string()
-    password = random_lower_string()
-    user_in = UserCreate(username=email, email=email, password=password)
-    user = crud.user.create(db_session=db_session, user_in=user_in)
-    return user
+    password = fake.password()
+
+    # generate random data for user
+    user_in = UserCreate(
+        email=fake.email(),
+        full_name=fake.name(),
+        password=password,
+        created_at=fake.date_time_this_month(),
+    )
+
+    # create and return user
+    # the generated password is added to the object for conveniency
+    # it will disappear permanently once the object will be garbage-collected
+    created = crud.user.create(db_session=db_session, user_in=user_in)
+    created.password = password
+    return created
