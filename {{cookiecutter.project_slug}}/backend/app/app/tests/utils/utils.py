541465d1030ca11c7dd33a71aaa2aea170f3e148
kedod <kedod@protonmail.com>
2019-06-13 16:00:10 +0200

    modify tests

--- {{cookiecutter.project_slug}}/backend/app/app/tests/utils/utils.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/utils/utils.py
@@ -2,12 +2,15 @@ import random
 import string
 
 import requests
-
 from app.core import config
 
 
 def random_lower_string():
     return "".join(random.choices(string.ascii_lowercase, k=32))
+
+
+def random_email():
+    return f"{''.join(random.choices(string.ascii_lowercase, k=32))}@mail.com"
 
 
 def get_server_api():
