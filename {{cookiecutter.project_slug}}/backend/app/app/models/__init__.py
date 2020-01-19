359581f2db3e4f65aaac561cd86e2691cd8d530a
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-19 21:59:46 +0100

    :rewind: Revert model changes, to have the minimum changes

--- {{cookiecutter.project_slug}}/backend/app/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# WATCH OUT
-#
-# load SQLalchemy classes, to avoid exception further down, such as:
-# sqlalchemy.exc.InvalidRequestError: When initializing mapper mapped class User->users,
-# expression 'APIKey' failed to locate a name ("name 'APIKey' is not defined"). If this is a class name,
-# consider adding this relationship() to the <class 'models_user.User'>
-# class after both dependent classes have been defined.
-from app.db import base  # noqa
--- {{cookiecutter.project_slug}}/backend/app/app/models/item.py
+++ {{cookiecutter.project_slug}}/backend/app/app/models/item.py
@@ -8,7 +8,5 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-
     owner_id = Column(Integer, ForeignKey("user.id"))
     owner = relationship("User", back_populates="items")
-    subitems = relationship("SubItem", back_populates="item")
