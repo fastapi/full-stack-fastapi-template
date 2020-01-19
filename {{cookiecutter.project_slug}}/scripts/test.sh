92ad76cf1dd52e05dad8e66a656f2f939c5be243
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-19 21:57:44 +0100

    :wrench: Update testing scripts

--- {{cookiecutter.project_slug}}/scripts/test.sh
+++ {{cookiecutter.project_slug}}/scripts/test.sh
@@ -9,12 +9,11 @@ docker-compose \
 -f docker-compose.shared.env.yml \
 -f docker-compose.shared.depends.yml \
 -f docker-compose.deploy.build.yml \
--f docker-compose.dev.volumes.yml \
 -f docker-compose.test.yml \
 config > docker-stack.yml
 
 docker-compose -f docker-stack.yml build
 docker-compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 docker-compose -f docker-stack.yml up -d
-docker-compose -f docker-stack.yml exec -T backend-tests /tests-start.sh $*
+docker-compose -f docker-stack.yml exec -T backend-tests /tests-start.sh "$@"
 docker-compose -f docker-stack.yml down -v --remove-orphans
