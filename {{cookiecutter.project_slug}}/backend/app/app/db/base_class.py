4d6de8cfaf10c0b3326d01b414a10a61900c907a
Sebastián Ramírez <tiangolo@gmail.com>
2020-01-19 22:00:49 +0100

    :rewind: Revert DB base and changes, separate CRUD from DB models

--- {{cookiecutter.project_slug}}/backend/app/app/db/base_class.py
+++ {{cookiecutter.project_slug}}/backend/app/app/db/base_class.py
@@ -1,4 +1,3 @@
-from fastapi.encoders import jsonable_encoder
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
 
 
@@ -7,13 +6,6 @@ class CustomBase(object):
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
-
-    def to_schema(self, schema_cls):
-        return schema_cls(**self.__dict__)
-
-    @classmethod
-    def from_schema(cls, schema_obj):
-        return cls(**jsonable_encoder(schema_obj))
 
 
 Base = declarative_base(cls=CustomBase)
