acfddcbf75e78c4236f450fa19b8c1d2d7e1f602
Mocsar Kalman <mocsar.kalman@gravityrd.com>
2020-02-04 11:49:36 +0100

    Bug fixes in backend app

--- {{cookiecutter.project_slug}}/backend/app/app/crud/base.py
+++ {{cookiecutter.project_slug}}/backend/app/app/crud/base.py
@@ -37,11 +37,9 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         db_session.refresh(db_obj)
         return db_obj
 
-    def update(
-        self, db_session: Session, *, db_obj: ModelType, obj_in: UpdateSchemaType
-    ) -> ModelType:
+    def update(self, db_session: Session, *, db_obj: ModelType, obj_in: UpdateSchemaType) -> ModelType:
         obj_data = jsonable_encoder(db_obj)
-        update_data = obj_in.dict(skip_defaults=True)
+        update_data = obj_in.dict(exclude_unset=True)
         for field in obj_data:
             if field in update_data:
                 setattr(db_obj, field, update_data[field])
