32263f9fb45eb17d18b7e2f1d152c3a728b46a8d
Brendon Smith <br3ndonland@protonmail.com>
2020-02-25 16:44:21 -0500

    Bring line length into compliance with Black
    Should be 88 characters.

--- {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/utils.py
+++ {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/utils.py
@@ -12,9 +12,7 @@ router = APIRouter()
 
 
 @router.post("/test-celery/", response_model=Msg, status_code=201)
-def test_celery(
-    msg: Msg, current_user: DBUser = Depends(get_current_active_superuser)
-):
+def test_celery(msg: Msg, current_user: DBUser = Depends(get_current_active_superuser)):
     """
     Test Celery worker.
     """
