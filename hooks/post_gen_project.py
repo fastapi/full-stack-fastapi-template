9cd4eec86bb71a3a4567fa32c3c39130a5ce2686
alexmitelman <alexmitelman@gmail.com>
2020-03-07 13:36:56 +0200

    Better naming

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 
-files_to_ignore = '''
+files_to_ignore_text = '''
 # Warning! Files in the below section contain your credentials!
 # Credentials should never be pushed to the repository.
 cookiecutter-config-file.yml
@@ -14,7 +14,7 @@ if not gitignore.exists():
     # We should assume this file exists otherwise something went wrong
     sys.exit(1)
 
-existing_gitignore = gitignore.read_text()
-gitignore.write_text(existing_gitignore + files_to_ignore)
+existing_gitignore_text = gitignore.read_text()
+gitignore.write_text(existing_gitignore_text + files_to_ignore_text)
 
 sys.exit(0)
