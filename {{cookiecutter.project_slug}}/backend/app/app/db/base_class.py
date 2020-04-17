61992727a169a83bfdad93b9008f93d5acb15c1d
Sebastián Ramírez <tiangolo@gmail.com>
2020-04-17 09:29:02 +0200

    :recycle: Remove object inheritance

--- {{cookiecutter.project_slug}}/backend/app/app/db/base_class.py
+++ {{cookiecutter.project_slug}}/backend/app/app/db/base_class.py
@@ -2,9 +2,8 @@ from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
 @as_declarative()
-class Base(object):
+class Base:
     # Generate __tablename__ automatically
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
-
