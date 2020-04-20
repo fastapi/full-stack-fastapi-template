0bd332ee37380660aef5adffc3ba69c6295ee402
Sebastián Ramírez <tiangolo@gmail.com>
2020-04-20 15:48:17 +0200

    :wrench: Update lint and format configs

--- {{cookiecutter.project_slug}}/backend/app/scripts/lint.sh
+++ {{cookiecutter.project_slug}}/backend/app/scripts/lint.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
 set -x
 
 mypy app
 black app --check
-isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --check-only app
+isort --recursive --check-only app
 vulture app --min-confidence 70
-flake8 --max-line-length 88 --exclude=__init__.py
+flake8
