fastapi/fastapi

commit 6fdba196397d9896bce0fee80bbb15c479d40e45

:sparkles: Update all for Postgres and new techniques

{{cookiecutter.project_slug}}/backend/app/app/utils.py
@@ -1,96 +1,86 @@
 import logging
 from datetime import datetime, timedelta
 from pathlib import Path
-from typing import Union
+from typing import Optional
 
 import emails
 import jwt
 from emails.template import JinjaTemplate
 from jwt.exceptions import InvalidTokenError
 
-from app.core.config import (
-    EMAIL_RESET_TOKEN_EXPIRE_HOURS,
-    EMAIL_TEMPLATES_DIR,
-    EMAILS_ENABLED,
-    EMAILS_FROM_EMAIL,
-    EMAILS_FROM_NAME,
-    PROJECT_NAME,
-    SECRET_KEY,
-    SERVER_HOST,
-    SMTP_HOST,
-    SMTP_PASSWORD,
-    SMTP_PORT,
-    SMTP_TLS,
-    SMTP_USER,
-)
+from app.core import config
 
 password_reset_jwt_subject = "preset"
 
 
 def send_email(email_to: str, subject_template="", html_template="", environment={}):
-    assert EMAILS_ENABLED, "no provided configuration for email variables"
+    assert config.EMAILS_ENABLED, "no provided configuration for email variables"
     message = emails.Message(
         subject=JinjaTemplate(subject_template),
         html=JinjaTemplate(html_template),
-        mail_from=(EMAILS_FROM_NAME, EMAILS_FROM_EMAIL),
+        mail_from=(config.EMAILS_FROM_NAME, config.EMAILS_FROM_EMAIL),
     )
-    smtp_options = {"host": SMTP_HOST, "port": SMTP_PORT}
-    if SMTP_TLS:
+    smtp_options = {"host": config.SMTP_HOST, "port": config.SMTP_PORT}
+    if config.SMTP_TLS:
         smtp_options["tls"] = True
-    if SMTP_USER:
-        smtp_options["user"] = SMTP_USER
-    if SMTP_PASSWORD:
-        smtp_options["password"] = SMTP_PASSWORD
+    if config.SMTP_USER:
+        smtp_options["user"] = config.SMTP_USER
+    if config.SMTP_PASSWORD:
+        smtp_options["password"] = config.SMTP_PASSWORD
     response = message.send(to=email_to, render=environment, smtp=smtp_options)
     logging.info(f"send email result: {response}")
 
 
 def send_test_email(email_to: str):
-    subject = f"{PROJECT_NAME} - Test email"
-    with open(Path(EMAIL_TEMPLATES_DIR) / "test_email.html") as f:
+    project_name = config.PROJECT_NAME
+    subject = f"{project_name} - Test email"
+    with open(Path(config.EMAIL_TEMPLATES_DIR) / "test_email.html") as f:
         template_str = f.read()
     send_email(
         email_to=email_to,
         subject_template=subject,
         html_template=template_str,
-        environment={"project_name": PROJECT_NAME, "email": email_to},
+        environment={"project_name": config.PROJECT_NAME, "email": email_to},
     )
 
 
-def send_reset_password_email(email_to: str, username: str, token: str):
-    subject = f"{PROJECT_NAME} - Password recovery for user {username}"
-    with open(Path(EMAIL_TEMPLATES_DIR) / "reset_password.html") as f:
+def send_reset_password_email(email_to: str, email: str, token: str):
+    project_name = config.PROJECT_NAME
+    subject = f"{project_name} - Password recovery for user {email}"
+    with open(Path(config.EMAIL_TEMPLATES_DIR) / "reset_password.html") as f:
         template_str = f.read()
     if hasattr(token, "decode"):
         use_token = token.decode()
     else:
         use_token = token
-    link = f"{SERVER_HOST}/reset-password?token={use_token}"
+    server_host = config.SERVER_HOST
+    link = f"{server_host}/reset-password?token={use_token}"
     send_email(
         email_to=email_to,
         subject_template=subject,
         html_template=template_str,
         environment={
-            "project_name": PROJECT_NAME,
-            "username": username,
+            "project_name": config.PROJECT_NAME,
+            "username": email,
             "email": email_to,
-            "valid_hours": EMAIL_RESET_TOKEN_EXPIRE_HOURS,
+            "valid_hours": config.EMAIL_RESET_TOKEN_EXPIRE_HOURS,
             "link": link,
         },
     )
 
 
 def send_new_account_email(email_to: str, username: str, password: str):
-    subject = f"{PROJECT_NAME} - New acccount for user {username}"
-    with open(Path(EMAIL_TEMPLATES_DIR) / "new_account.html") as f:
+    project_name = config.PROJECT_NAME
+    subject = f"{project_name} - New acccount for user {username}"
+    with open(Path(config.EMAIL_TEMPLATES_DIR) / "new_account.html") as f:
         template_str = f.read()
-    link = f"{SERVER_HOST}"
+    link = config.SERVER_HOST
     send_email(
         email_to=email_to,
         subject_template=subject,
         html_template=template_str,
         environment={
-            "project_name": PROJECT_NAME,
+            "project_name": config.PROJECT_NAME,
             "username": username,
             "password": password,
             "email": email_to,
@@ -99,28 +89,23 @@ def send_new_account_email(email_to: str, username: str, password: str):
     )
 
 
-def generate_password_reset_token(username):
-    delta = timedelta(hours=EMAIL_RESET_TOKEN_EXPIRE_HOURS)
+def generate_password_reset_token(email):
+    delta = timedelta(hours=config.EMAIL_RESET_TOKEN_EXPIRE_HOURS)
     now = datetime.utcnow()
     expires = now + delta
     exp = expires.timestamp()
     encoded_jwt = jwt.encode(
-        {
-            "exp": exp,
-            "nbf": now,
-            "sub": password_reset_jwt_subject,
-            "username": username,
-        },
-        SECRET_KEY,
+        {"exp": exp, "nbf": now, "sub": password_reset_jwt_subject, "email": email},
+        config.SECRET_KEY,
         algorithm="HS256",
     )
     return encoded_jwt
 
 
-def verify_password_reset_token(token) -> Union[str, bool]:
+def verify_password_reset_token(token) -> Optional[str]:
     try:
-        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        decoded_token = jwt.decode(token, config.SECRET_KEY, algorithms=["HS256"])
         assert decoded_token["sub"] == password_reset_jwt_subject
-        return decoded_token["username"]
+        return decoded_token["email"]
     except InvalidTokenError:
-        return False
+        return None

{{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/utils.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic.types import EmailStr
-from starlette.exceptions import HTTPException
 
+from app.api.utils.security import get_current_user
 from app.core.celery_app import celery_app
-from app.core.jwt import get_current_user
-from app.crud.user import check_if_user_is_superuser
+from app.crud import user as crud_user
 from app.models.msg import Msg
 from app.models.user import UserInDB
 from app.utils import send_test_email
@@ -13,24 +12,22 @@ router = APIRouter()
 
 
 @router.post("/test-celery/", tags=["utils"], response_model=Msg, status_code=201)
-def route_test_celery(msg: Msg, current_user: UserInDB = Depends(get_current_user)):
+def test_celery(msg: Msg, current_user: UserInDB = Depends(get_current_user)):
     """
     Test Celery worker
     """
-    if not check_if_user_is_superuser(current_user):
+    if not crud_user.is_superuser(current_user):
         raise HTTPException(status_code=400, detail="Not a superuser")
     celery_app.send_task("app.worker.test_celery", args=[msg.msg])
     return {"msg": "Word received"}
 
 
 @router.post("/test-email/", tags=["utils"], response_model=Msg, status_code=201)
-def route_test_email(
-    email_to: EmailStr, current_user: UserInDB = Depends(get_current_user)
-):
+def test_email(email_to: EmailStr, current_user: UserInDB = Depends(get_current_user)):
     """
     Test emails
     """
-    if not check_if_user_is_superuser(current_user):
+    if not crud_user.is_superuser(current_user):
         raise HTTPException(status_code=400, detail="Not a superuser")
     send_test_email(email_to=email_to)
     return {"msg": "Test email sent"}

{{cookiecutter.project_slug}}/backend/app/app/crud/utils.py
@@ -1,214 +1,0 @@
-import uuid
-from enum import Enum
-from typing import List, Sequence, Type, Union
-
-from pydantic import BaseModel
-from pydantic.fields import Field, Shape
-
-from app.core.config import COUCHBASE_BUCKET_NAME
-from couchbase.bucket import Bucket
-from couchbase.fulltext import MatchAllQuery, QueryStringQuery
-from couchbase.n1ql import CONSISTENCY_REQUEST, N1QLQuery
-
-
-def generate_new_id():
-    return str(uuid.uuid4())
-
-
-def ensure_enums_to_strs(items: Union[Sequence[Union[Enum, str]], Type[Enum]]):
-    str_items = []
-    for item in items:
-        if isinstance(item, Enum):
-            str_items.append(str(item.value))
-        else:
-            str_items.append(str(item))
-    return str_items
-
-
-def get_all_documents_by_type(bucket: Bucket, *, doc_type: str, skip=0, limit=100):
-    query_str = f"SELECT *, META().id as id FROM {COUCHBASE_BUCKET_NAME} WHERE type = $type LIMIT $limit OFFSET $skip;"
-    q = N1QLQuery(
-        query_str, bucket=COUCHBASE_BUCKET_NAME, type=doc_type, limit=limit, skip=skip
-    )
-    q.consistency = CONSISTENCY_REQUEST
-    result = bucket.n1ql_query(q)
-    return result
-
-
-def get_documents_by_keys(
-    bucket: Bucket, *, keys: List[str], doc_model=Type[BaseModel]
-):
-    results = bucket.get_multi(keys, quiet=True)
-    docs = []
-    for result in results.values():
-        doc = doc_model(**result.value)
-        docs.append(doc)
-    return docs
-
-
-def results_to_model(results_from_couchbase: list, *, doc_model: Type[BaseModel]):
-    items = []
-    for doc in results_from_couchbase:
-        data = doc[COUCHBASE_BUCKET_NAME]
-        doc = doc_model(**data)
-        items.append(doc)
-    return items
-
-
-def search_results_to_model(
-    results_from_couchbase: list, *, doc_model: Type[BaseModel]
-):
-    items = []
-    for doc in results_from_couchbase:
-        data = doc.get("fields")
-        if not data:
-            continue
-        data_nones = {}
-        for key, value in data.items():
-            field: Field = doc_model.__fields__[key]
-            if not value:
-                value = None
-            elif field.shape in {Shape.LIST, Shape.SET, Shape.TUPLE} and not isinstance(
-                value, list
-            ):
-                value = [value]
-            data_nones[key] = value
-        doc = doc_model(**data_nones)
-        items.append(doc)
-    return items
-
-
-def get_docs(
-    bucket: Bucket, *, doc_type: str, doc_model=Type[BaseModel], skip=0, limit=100
-):
-    doc_results = get_all_documents_by_type(
-        bucket, doc_type=doc_type, skip=skip, limit=limit
-    )
-    return results_to_model(doc_results, doc_model=doc_model)
-
-
-def get_doc(bucket: Bucket, *, doc_id: str, doc_model: Type[BaseModel]):
-    result = bucket.get(doc_id, quiet=True)
-    if not result.value:
-        return None
-    model = doc_model(**result.value)
-    return model
-
-
-def search_docs_get_doc_ids(
-    bucket: Bucket,
-    *,
-    query_string: str,
-    index_name: str,
-    skip: int = 0,
-    limit: int = 100,
-):
-    query = QueryStringQuery(query_string)
-    hits = bucket.search(index_name, query, skip=skip, limit=limit)
-    doc_ids = []
-    for hit in hits:
-        doc_ids.append(hit["id"])
-    return doc_ids
-
-
-def search_get_results(
-    bucket: Bucket,
-    *,
-    query_string: str,
-    index_name: str,
-    skip: int = 0,
-    limit: int = 100,
-):
-    if query_string:
-        query = QueryStringQuery(query_string)
-    else:
-        query = MatchAllQuery()
-    hits = bucket.search(index_name, query, fields=["*"], skip=skip, limit=limit)
-    docs = []
-    for hit in hits:
-        docs.append(hit)
-    return docs
-
-
-def search_get_results_by_type(
-    bucket: Bucket,
-    *,
-    query_string: str,
-    index_name: str,
-    doc_type: str,
-    skip: int = 0,
-    limit: int = 100,
-):
-    type_filter = f"type:{doc_type}"
-    if not query_string:
-        query_string = type_filter
-    if query_string and type_filter not in query_string:
-        query_string += f" {type_filter}"
-    query = QueryStringQuery(query_string)
-    hits = bucket.search(index_name, query, fields=["*"], skip=skip, limit=limit)
-    docs = []
-    for hit in hits:
-        docs.append(hit)
-    return docs
-
-
-def search_docs(
-    bucket: Bucket,
-    *,
-    query_string: str,
-    index_name: str,
-    doc_model: Type[BaseModel],
-    skip=0,
-    limit=100,
-):
-    keys = search_docs_get_doc_ids(
-        bucket=bucket,
-        query_string=query_string,
-        index_name=index_name,
-        skip=skip,
-        limit=limit,
-    )
-    if not keys:
-        return []
-    doc_results = get_documents_by_keys(bucket=bucket, keys=keys, doc_model=doc_model)
-    return doc_results
-
-
-def search_results(
-    bucket: Bucket,
-    *,
-    query_string: str,
-    index_name: str,
-    doc_model: Type[BaseModel],
-    skip=0,
-    limit=100,
-):
-    doc_results = search_get_results(
-        bucket=bucket,
-        query_string=query_string,
-        index_name=index_name,
-        skip=skip,
-        limit=limit,
-    )
-    return search_results_to_model(doc_results, doc_model=doc_model)
-
-
-def search_results_by_type(
-    bucket: Bucket,
-    *,
-    query_string: str,
-    index_name: str,
-    doc_type: str,
-    doc_model: Type[BaseModel],
-    skip=0,
-    limit=100,
-):
-    doc_results = search_get_results_by_type(
-        bucket=bucket,
-        query_string=query_string,
-        index_name=index_name,
-        doc_type=doc_type,
-        skip=skip,
-        limit=limit,
-    )
-    return search_results_to_model(doc_results, doc_model=doc_model)

fastapi/full-stack-fastapi-template

commit 423e246c41807f044c0a249e65eb43bef3642d89

issue#171 decoded jwt token returns 'email' which is not present, now returned 'sub'. probably caused by 6fdba196397d9896bce0fee80bbb15c479d40e45

{{cookiecutter.project_slug}}/backend/app/app/utils.py
@@ -101,6 +101,6 @@ def generate_password_reset_token(email: str) -> str:
 def verify_password_reset_token(token: str) -> Optional[str]:
     try:
         decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-        return decoded_token["email"]
+        return decoded_token["sub"]
     except jwt.JWTError:
         return None