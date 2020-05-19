cda05404048c7cfb90b1d0ec138ed6ece045ae12
pteroid <tohokamiemitame4185@gmail.com>
2020-05-19 16:10:08 +0900

    use `--ignore-init-module-imports` to autoflake

--- {{cookiecutter.project_slug}}/backend/app/scripts/format.sh
+++ {{cookiecutter.project_slug}}/backend/app/scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude=__init__.py
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --ignore-init-module-imports
 black app
 isort --recursive --apply app
