3717265f97c134df409576b020e8faead1ff3861
Ruslan Samoylov <ruslan.v.samoylov@gmail.com>
2020-05-26 03:13:54 +0300

    black formatting

--- {{cookiecutter.project_slug}}/backend/app/app/crud/base.py
+++ {{cookiecutter.project_slug}}/backend/app/app/crud/base.py
@@ -44,7 +44,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         db: Session,
         *,
         db_obj: ModelType,
-        obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+        obj_in: Union[UpdateSchemaType, Dict[str, Any]],
     ) -> ModelType:
         obj_data = jsonable_encoder(db_obj)
         if isinstance(obj_in, dict):
