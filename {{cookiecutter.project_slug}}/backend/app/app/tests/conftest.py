1d305cb83dba28ba4cacba613d1dc6edac7a258d
Bruno Inec <brunoinec@gmail.com>
2020-06-26 15:52:08 +0200

    Run each test in there own transaction

--- {{cookiecutter.project_slug}}/backend/app/app/tests/conftest.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/conftest.py
@@ -4,16 +4,30 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
+from app.api.deps import get_db
 from app.core.config import settings
-from app.db.session import SessionLocal
+from app.db.session import engine
 from app.main import app
 from app.tests.utils.user import authentication_token_from_email
 from app.tests.utils.utils import get_superuser_token_headers
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def db() -> Generator:
-    yield SessionLocal()
+    """Run each test in there own transaction"""
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = Session(bind=connection, autocommit=False, autoflush=False)
+
+    app.dependency_overrides[get_db] = lambda: session
+
+    yield session
+
+    app.dependency_overrides.pop(get_db)
+
+    session.close()
+    transaction.rollback()
+    connection.close()
 
 
 @pytest.fixture(scope="module")
