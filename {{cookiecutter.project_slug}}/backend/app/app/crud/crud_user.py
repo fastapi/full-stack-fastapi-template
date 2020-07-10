2e90990b854355dce5f12cf1ea60091339b76678
ivandjuricic <ivand@playstudios-il.com>
2020-07-10 17:04:48 +0200

    Fix KeyError on updating user object without password

--- {{cookiecutter.project_slug}}/backend/app/app/crud/crud_user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/crud/crud_user.py
@@ -31,7 +31,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             update_data = obj_in
         else:
             update_data = obj_in.dict(exclude_unset=True)
-        if update_data["password"]:
+        if "password" in update_data:
             hashed_password = get_password_hash(update_data["password"])
             del update_data["password"]
             update_data["hashed_password"] = hashed_password
