78e7d6216b8ad9a8f6779410fe380051ecd96678
Douglas <machadoug@users.noreply.github.com>
2020-08-20 08:10:14 -0300

    Better http error codes
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/403
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401

--- {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/login.py
+++ {{cookiecutter.project_slug}}/backend/app/app/api/api_v1/endpoints/login.py
@@ -30,9 +30,9 @@ def login_access_token(
         db, email=form_data.username, password=form_data.password
     )
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=403, detail="Incorrect email or password")
     elif not crud.user.is_active(user):
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=401, detail="Inactive user")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": security.create_access_token(
@@ -88,7 +88,7 @@ def reset_password(
             detail="The user with this username does not exist in the system.",
         )
     elif not crud.user.is_active(user):
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=401, detail="Inactive user")
     hashed_password = get_password_hash(new_password)
     user.hashed_password = hashed_password
     db.add(user)
