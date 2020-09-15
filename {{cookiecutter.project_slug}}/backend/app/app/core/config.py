5da9074eb1182d490f5e804c75c8f637fb7cd93e
Orkhan <rblbl@mail.ru>
2020-09-15 10:28:34 +0300

    Fix checking length of None object

--- {{cookiecutter.project_slug}}/backend/app/app/core/config.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/config.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
 
     @validator("SENTRY_DSN", pre=True)
     def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
-        if len(v) == 0:
+        if v and len(v) == 0:
             return None
         return v
 
