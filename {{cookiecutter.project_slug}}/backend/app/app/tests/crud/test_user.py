30fe5c057ed9a66846b64d38d77d7eea3b3088c9
michaeltoohig <34083701+michaeltoohig@users.noreply.github.com>
2020-09-22 10:33:58 +1100

    Update user test

--- {{cookiecutter.project_slug}}/backend/app/app/tests/crud/test_user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/crud/test_user.py
@@ -92,3 +92,17 @@ def test_update_user(db: Session) -> None:
     assert user_2
     assert user.email == user_2.email
     assert verify_password(new_password, user_2.hashed_password)
+
+
+def test_update_user_not_password(db: Session) -> None:
+    password = random_lower_string()
+    email = random_email()
+    user_in = UserCreate(email=email, password=password, is_superuser=True)
+    user = crud.user.create(db, obj_in=user_in)
+    full_name = random_lower_string()
+    user_in_update = UserUpdate(full_name=full_name, is_superuser=True)
+    crud.user.update(db, db_obj=user, obj_in=user_in_update)
+    user_2 = crud.user.get(db, id=user.id)
+    assert user_2
+    assert user.email == user_2.email
+    assert user.full_name == user_2.full_name
