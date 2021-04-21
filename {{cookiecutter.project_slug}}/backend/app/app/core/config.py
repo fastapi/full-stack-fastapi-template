170193b19dc4f052e9d34dc799e43c98c9b0dba1
Roberto Perdomo Macedo <robertop23@gmail.com>
2021-04-21 12:59:40 +0100

    Update config.py
    ### I usually use the Python shell when I'm developing, but I cannot simple open a Python shell using the env vars because I get this error:
        pydantic.env_settings.SettingsError: error parsing JSON for "BACKEND_CORS_ORIGINS"
    
    ### How to reproduce this error:
        cd backend/app/
        export $(grep -v '^#' ../../.env | tr -d ' ' | xargs) && poetry run python
    > And inside the Python terminal:
    
        from app.core.config import settings

--- {{cookiecutter.project_slug}}/backend/app/app/core/config.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: Union[str, List[AnyHttpUrl]] = Field(..., env="BACKEND_CORS_ORIGINS")
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
