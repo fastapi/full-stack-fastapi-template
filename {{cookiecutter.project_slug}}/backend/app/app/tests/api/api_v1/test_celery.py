6afbff033a56766241dde1d9ef0f1c3dbc5de74f
ghostformobile <ghostformobile@gmail.com>
2021-11-27 15:13:41 +0300

    add async to test celery endpoint

--- {{cookiecutter.project_slug}}/backend/app/app/tests/api/api_v1/test_celery.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/api/api_v1/test_celery.py
@@ -1,15 +1,16 @@
 from typing import Dict
-
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
 
 from app.core.config import settings
 
+pytestmark = pytest.mark.asyncio
 
-def test_celery_worker_test(
-    client: TestClient, superuser_token_headers: Dict[str, str]
+async def test_celery_worker_test(
+    client: AsyncClient, superuser_token_headers: Dict[str, str]
 ) -> None:
     data = {"msg": "test"}
-    r = client.post(
+    r = await client.post(
         f"{settings.API_V1_STR}/utils/test-celery/",
         json=data,
         headers=superuser_token_headers,
