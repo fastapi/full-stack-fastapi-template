933d1ce74c8164fa27a4b4fa37aea4545db08a3a
ghostformobile <ghostformobile@gmail.com>
2021-11-27 14:06:58 +0300

    add async to tests_pre_start

--- {{cookiecutter.project_slug}}/backend/app/app/tests_pre_start.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests_pre_start.py
@@ -1,8 +1,12 @@
 import logging
-
+import asyncio
 from tenacity import after_log, before_log, retry, stop_after_attempt, wait_fixed
 
-from app.db.session import SessionLocal
+#from app.db.session import SessionLocal
+from app.db.session import async_session, engine_async
+from app.db import base  # noqa: F401
+from app.db.init_db import init_db
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -17,21 +21,29 @@ wait_seconds = 1
     before=before_log(logger, logging.INFO),
     after=after_log(logger, logging.WARN),
 )
-def init() -> None:
+async def init() -> None:
     try:
         # Try to create session to check if DB is awake
-        db = SessionLocal()
-        db.execute("SELECT 1")
+        #db = SessionLocal()
+        async with engine_async.begin() as conn:
+            logger.info("DROP DATABASE")
+            await conn.run_sync(base.Base.metadata.drop_all)
+            logger.info("CREATE DATABASE")
+            await conn.run_sync(base.Base.metadata.create_all)
+        db = async_session()
+        await init_db(db=db)
+        await db.execute("SELECT 1")
+        logger.info("DATABASE DONE")
     except Exception as e:
         logger.error(e)
         raise e
 
 
-def main() -> None:
+async def main() -> None:
     logger.info("Initializing service")
-    init()
+    await init()
     logger.info("Service finished initializing")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
