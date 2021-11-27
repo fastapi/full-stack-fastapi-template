de2768a4742796555ae1eb7ad0c6310096610828
ghostformobile <ghostformobile@gmail.com>
2021-11-27 14:07:16 +0300

    add async to initial_data

--- {{cookiecutter.project_slug}}/backend/app/app/initial_data.py
+++ {{cookiecutter.project_slug}}/backend/app/app/initial_data.py
@@ -1,22 +1,24 @@
 import logging
+import asyncio
 
 from app.db.init_db import init_db
-from app.db.session import SessionLocal
+#from app.db.session import SessionLocal
+from app.db.session import async_session
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def init() -> None:
-    db = SessionLocal()
-    init_db(db)
+async def init() -> None:
+    async with async_session() as db:
+        await init_db(db)
 
 
-def main() -> None:
+async def main() -> None:
     logger.info("Creating initial data")
-    init()
+    await init()
     logger.info("Initial data created")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
