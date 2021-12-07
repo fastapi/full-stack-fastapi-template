651510714e5befb2d24b72d62d577619d413bcfc
ghostformobile <ghostformobile@gmail.com>
2021-12-07 21:50:53 +0300

    edit alembic for tests

--- {{cookiecutter.project_slug}}/backend/app/alembic/env.py
+++ {{cookiecutter.project_slug}}/backend/app/alembic/env.py
@@ -34,7 +34,11 @@ def get_url():
     user = os.getenv("POSTGRES_USER", "postgres")
     password = os.getenv("POSTGRES_PASSWORD", "")
     server = os.getenv("POSTGRES_SERVER", "db")
-    db = os.getenv("POSTGRES_DB", "app")
+    TEST_MODE = os.getenv("TEST_MODE", False)
+    if TEST_MODE:
+        db = "postgres"
+    else:
+        db = os.getenv("POSTGRES_DB", "app")
     return f"postgresql://{user}:{password}@{server}/{db}"
 
 
