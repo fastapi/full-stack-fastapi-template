ee35fb92dadc9a12e044dc5bfd0702ef44769ad9
longniao <longniao@gmail.com>
2022-02-07 10:39:49 -0600

    fix bug on update data in curd orm

--- {{cookiecutter.project_slug}}/backend/app/app/crud/base.py
+++ {{cookiecutter.project_slug}}/backend/app/app/crud/base.py
@@ -54,7 +54,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         for field in obj_data:
             if field in update_data:
                 setattr(db_obj, field, update_data[field])
-        db.add(db_obj)
+        db.merge(db_obj)
         db.commit()
         db.refresh(db_obj)
         return db_obj
