a5cd22be6a2ed2a260ee1abc541d44939bdccad0
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-12-22 13:17:04 +0500

    init

--- {{cookiecutter.project_slug}}/backend/app/app/crud/crud_user.py
+++ {{cookiecutter.project_slug}}/backend/app/app/crud/crud_user.py
@@ -31,7 +31,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             update_data = obj_in
         else:
             update_data = obj_in.dict(exclude_unset=True)
-        if update_data["password"]:
+        if update_data.get("password"):
             hashed_password = get_password_hash(update_data["password"])
             del update_data["password"]
             update_data["hashed_password"] = hashed_password
