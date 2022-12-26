7517b38acca493c5c0b215fef3968a24673ad321
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-12-26 13:47:08 +0500

    add async to crud models

--- {{cookiecutter.project_slug}}/backend/app/app/crud/base.py
+++ {{cookiecutter.project_slug}}/backend/app/app/crud/base.py
@@ -3,8 +3,10 @@ from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union, cas
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.base_class import Base
+from sqlalchemy.sql.expression import select
 
 ModelType = TypeVar("ModelType", bound=Base)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
@@ -23,28 +25,44 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         """
         self.model = model
 
-    def get(self, db: Session, id: Any) -> Optional[ModelType]:
-        return db.query(self.model).filter(self.model.id == id).first()
+    async def get(self, db: AsyncSession, id: Any) -> Optional[ModelType]:
+        result = await db.execute(select(self.model).filter(self.model.id == id))
+        return result.scalars().first()
 
-    def get_multi(
-        self, db: Session, *, skip: int = 0, limit: int = 100
+    async def get_multi(
+            self, db: AsyncSession, *, skip: int = 0, limit: int = 100
     ) -> List[ModelType]:
-        return db.query(self.model).offset(skip).limit(limit).all()
+        query = select(self.model).offset(skip).limit(limit)
+        result = await db.execute(query)
+        res = result.scalars().all()
+        return res
 
-    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
-        obj_in_data = jsonable_encoder(obj_in)
+    async def get_multi_by_owner(
+            self, db: AsyncSession, *, owner_id: int, skip: int = 0, limit: int = 100
+    ) -> List[ModelType]:
+        result = await db.execute(
+            select(self.model)
+            .filter(self.model.owner_id == owner_id)
+            .offset(skip)
+            .limit(limit)
+        )
+        return result.scalars().all()
+
+    async def create(self, db: AsyncSession, *, obj_in: CreateSchemaType) -> ModelType:
+        # obj_in_data = jsonable_encoder(obj_in)
+        obj_in_data = obj_in.dict()
         db_obj = self.model(**obj_in_data)  # type: ignore
         db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
+        await db.commit()
+        await db.refresh(db_obj)
         return db_obj
 
-    def update(
-        self,
-        db: Session,
-        *,
-        db_obj: ModelType,
-        obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+    async def update(
+            self,
+            db: AsyncSession,
+            *,
+            db_obj: ModelType,
+            obj_in: Union[UpdateSchemaType, Dict[str, Any]]
     ) -> ModelType:
         obj_data = jsonable_encoder(db_obj)
         if isinstance(obj_in, dict):
@@ -55,13 +73,13 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
             if field in update_data:
                 setattr(db_obj, field, update_data[field])
         db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
+        await db.commit()
+        await db.refresh(db_obj)
         return db_obj
 
-    def remove(self, db: Session, *, id: int) -> ModelType:
-        obj = db.get(self.model, id)
+    async def remove(self, db: AsyncSession, *, id: int) -> ModelType:
+        obj = await db.get(self.model, id)
         assert obj is not None
-        db.delete(obj)
-        db.commit()
+        await db.delete(obj)
+        await db.commit()
         return cast(ModelType, obj)
