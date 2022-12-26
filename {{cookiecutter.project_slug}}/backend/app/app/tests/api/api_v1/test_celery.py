94a39bbf003bfbdc2a7b601c7edd742dfbcffec8
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2022-12-26 15:41:24 +0500

    add async to api tests

--- {{cookiecutter.project_slug}}/backend/app/app/tests/api/api_v1/test_celery.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/api/api_v1/test_celery.py
@@ -1,33 +1,20 @@
-import time
 from typing import Dict
-
-import kombu
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
 
 from app.core.config import settings
 
+pytestmark = pytest.mark.asyncio
 
-def test_celery_worker_test(
-    client: TestClient, superuser_token_headers: Dict[str, str]
+
+async def test_celery_worker_test(
+        client: AsyncClient, superuser_token_headers: Dict[str, str]
 ) -> None:
     data = {"msg": "test"}
-    retries = 1
-    r = None
-    while (retries + 1) > 0:
-        try:
-            r = client.post(
-                f"{settings.API_V1_STR}/utils/test-celery/",
-                json=data,
-                headers=superuser_token_headers,
-            )
-        except kombu.exceptions.OperationalError:
-            # This can happen right when the stack starts,
-            # give celery a couple seconds
-            time.sleep(5)
-            retries -= 1
-            continue
-        break
-
-    assert r
+    r = await client.post(
+        f"{settings.API_V1_STR}/utils/test-celery/",
+        json=data,
+        headers=superuser_token_headers,
+    )
     response = r.json()
     assert response["msg"] == "Word received"
