a60a256e3f0ce8f06ed71163c1c5bd090d65ca1f
Gavin Chait <whythawk@gmail.com>
2023-01-10 11:17:29 +0100

    Updated error in access token variable name

--- {{cookiecutter.project_slug}}/backend/app/app/core/config.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    ACCESS_TOKEN_EXPIRE_SECONDS: int = 60 * 24 * 8
     REFRESH_TOKEN_EXPIRE_SECONDS: int = 60 * 60 * 24 * 30
     SERVER_NAME: str
     SERVER_HOST: AnyHttpUrl
