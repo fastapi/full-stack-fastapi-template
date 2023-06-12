65fc6bc8b349cc186aadd98f81036500a8af01fd
Luis Felipe Junior Boscan Nava <lboscannava@gmail.com>
2023-06-12 17:47:38 -0400

    CI/CD removed

--- {{cookiecutter.project_slug}}/scripts/build-push.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-
-# Exit in case of error
-set -e
-
-TAG=${TAG?Variable not set} \
-sh ./scripts/build.sh
-
-docker-compose -f docker-compose.yml push
--- {{cookiecutter.project_slug}}/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env sh
-
-# Exit in case of error
-set -e
-
-TAG=${TAG?Variable not set} \
-docker-compose \
--f docker-compose.yml \
-build
