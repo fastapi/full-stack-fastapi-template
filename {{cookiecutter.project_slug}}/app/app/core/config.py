b81e25034a2770e062cc6cce360eae78c0b3c434
Luis Felipe Junior Boscan Nava <lboscannava@gmail.com>
2023-06-13 11:27:24 -0400

    Fixes

--- {{cookiecutter.project_slug}}/app/app/core/config.py
+++ {{cookiecutter.project_slug}}/app/app/core/config.py
@@ -2,7 +2,6 @@ import secrets
 from typing import Any, Dict, List, Optional, Union
 
 from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
-from pydantic.tools import parse_obj_as
 from sqlalchemy.engine import URL
 
 
@@ -11,7 +10,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    SERVER_HOST: AnyHttpUrl = parse_obj_as(AnyHttpUrl, "http://localhost:8080")
+    SERVER_HOST: str = "http://localhost:8080"
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080"]'
