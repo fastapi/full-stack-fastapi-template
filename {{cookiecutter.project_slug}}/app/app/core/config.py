478d9c53e1c04de33ed1d916bf4cf953fb78e504
Luis Felipe Junior Boscan Nava <lboscannava@gmail.com>
2023-06-13 11:20:25 -0400

    Minor fix

--- {{cookiecutter.project_slug}}/app/app/core/config.py
+++ {{cookiecutter.project_slug}}/app/app/core/config.py
@@ -38,7 +38,7 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
-    SQLALCHEMY_DATABASE_URI: Union[str, URL]
+    SQLALCHEMY_DATABASE_URI: Union[str, URL] = None  # type: ignore
 
     @validator("SQLALCHEMY_DATABASE_URI", pre=True)
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
