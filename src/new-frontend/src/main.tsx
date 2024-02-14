af31ff2e15004393c1edd518b4c4acde496b6597
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2024-02-14 10:41:58 -0500

    Remove ChakraProvider from main.tsx

--- src/new-frontend/src/main.tsx
+++ src/new-frontend/src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { ChakraProvider } from '@chakra-ui/react';
-
 import App from './App';
 import { OpenAPI } from './client';
 
@@ -13,9 +11,7 @@ OpenAPI.TOKEN = async () => {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
       <App />
-    </ChakraProvider>
   </React.StrictMode>,
 )
 
