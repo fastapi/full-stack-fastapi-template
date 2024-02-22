26ad9569c9fa0a59d0d52c7ebfb30c79d12c07ba
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2024-02-22 12:40:34 -0500

    Refactor UserSettings

--- src/new-frontend/src/pages/UserSettings.tsx
+++ src/new-frontend/src/pages/UserSettings.tsx
@@ -1,49 +1,40 @@
 import React from 'react';
 
 import { Container, Heading, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
-
 import Appearance from '../components/UserSettings/Appearance';
 import ChangePassword from '../components/UserSettings/ChangePassword';
 import DeleteAccount from '../components/UserSettings/DeleteAccount';
 import UserInformation from '../components/UserSettings/UserInformation';
 
-
+const tabsConfig = [
+    { title: 'My profile', component: UserInformation },
+    { title: 'Password', component: ChangePassword },
+    { title: 'Appearance', component: Appearance },
+    { title: 'Danger zone', component: DeleteAccount },
+];
 
 const UserSettings: React.FC = () => {
-
     return (
-        <>
-            <Container maxW='full'>
-                <Heading size='lg' textAlign={{ base: 'center', md: 'left' }} py={12}>
-                    User Settings
-                </Heading>
-                <Tabs variant='enclosed' >
-                    <TabList>
-                        <Tab>My profile</Tab>
-                        <Tab>Password</Tab>
-                        <Tab>Appearance</Tab>
-                        <Tab>Danger zone</Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <UserInformation />
+        <Container maxW='full'>
+            <Heading size='lg' textAlign={{ base: 'center', md: 'left' }} py={12}>
+                User Settings
+            </Heading>
+            <Tabs variant='enclosed'>
+                <TabList>
+                    {tabsConfig.map((tab, index) => (
+                        <Tab key={index}>{tab.title}</Tab>
+                    ))}
+                </TabList>
+                <TabPanels>
+                    {tabsConfig.map((tab, index) => (
+                        <TabPanel key={index}>
+                            <tab.component />
                         </TabPanel>
-                        <TabPanel>
-                            <ChangePassword />
-                        </TabPanel>
-                        <TabPanel>
-                            <Appearance />
-                        </TabPanel>
-                        <TabPanel>
-                            <DeleteAccount />
-                        </TabPanel>
-
-                    </TabPanels>
-                </Tabs>
-            </Container>
-        </>
+                    ))}
+                </TabPanels>
+            </Tabs>
+        </Container>
     );
 };
 
 export default UserSettings;
-
