c9d78dd4b8d3c9155cad9811ed782b9bd23b4248
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2024-03-05 12:58:03 -0500

    Adjust spacing

--- src/new-frontend/src/pages/Dashboard.tsx
+++ src/new-frontend/src/pages/Dashboard.tsx
@@ -5,8 +5,6 @@ import { useQueryClient } from 'react-query';
 
 import { UserOut } from '../client';
 
-
-
 const Dashboard: React.FC = () => {
     const queryClient = useQueryClient();
     const currentUser = queryClient.getQueryData<UserOut>('currentUser');
@@ -18,7 +16,6 @@ const Dashboard: React.FC = () => {
                 <Text>Welcome back, nice to see you again!</Text>
             </Container>
         </>
-
     )
 }
 
