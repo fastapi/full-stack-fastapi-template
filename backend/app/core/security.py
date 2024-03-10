393514862c29c781ebdf28ed7d04c17a10d46348
Sebastián Ramírez <tiangolo@gmail.com>
2024-03-10 20:30:32 +0100

    🔥 Remove code handling unused optional expired time for access token

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -13,12 +13,7 @@ ALGORITHM = "HS256"
 
 
 def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
+    expire = datetime.utcnow() + expires_delta
     to_encode = {"exp": expire, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
