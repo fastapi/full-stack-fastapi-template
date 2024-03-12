6d1e81f28abacca93149c99e53cf640caa7f231a
Sebastián Ramírez <tiangolo@gmail.com>
2024-03-12 13:59:38 +0100

    ♻️ Refactor to make only one of SMTP TLS or SSL enabled

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -38,7 +38,7 @@ def send_email(
     smtp_options = {"host": settings.SMTP_HOST, "port": settings.SMTP_PORT}
     if settings.SMTP_TLS:
         smtp_options["tls"] = True
-    if settings.SMTP_SSL:
+    elif settings.SMTP_SSL:
         smtp_options["ssl"] = True
     if settings.SMTP_USER:
         smtp_options["user"] = settings.SMTP_USER
