a50f5ed5173bd8896eb82854661dd8222830b0fd
Patrick Arminio <patrick.arminio@gmail.com>
2024-04-02 16:37:04 +0200

    Update scripts

--- backend/scripts/format.sh
+++ backend/scripts/format.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude=__init__.py
-black app
-isort --recursive --apply app
+ruff format
