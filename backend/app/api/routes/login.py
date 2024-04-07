a843a3fd1893446f1c2ebe7ccb0d5a6f490f91ee
Neveh A <nevehallon@gmail.com>
2024-04-07 16:55:13 +0300

    Update login.py

--- backend/app/api/routes/login.py
+++ backend/app/api/routes/login.py
@@ -38,7 +38,7 @@ def login_access_token(
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return Token(
         access_token=security.create_access_token(
-            user.id, expires_delta=access_token_expires
+            user.email, expires_delta=access_token_expires
         )
     )
 
