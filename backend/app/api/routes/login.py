32e680fc251e62b443eca4df276a927453b380c7
nevehallon <nevehallon@gmail.com>
2024-04-07 17:12:02 +0200

    refactor to get user by id from token sub

--- backend/app/api/routes/login.py
+++ backend/app/api/routes/login.py
@@ -10,7 +10,7 @@ from app.api.deps import CurrentUser, SessionDep, get_current_active_superuser
 from app.core import security
 from app.core.config import settings
 from app.core.security import get_password_hash
-from app.models import Message, NewPassword, Token, UserPublic
+from app.models import Message, NewPassword, Token, User, UserPublic
 from app.utils import (
     generate_password_reset_token,
     generate_reset_password_email,
@@ -38,7 +38,7 @@ def login_access_token(
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return Token(
         access_token=security.create_access_token(
-            user.email, expires_delta=access_token_expires
+            user.id, expires_delta=access_token_expires
         )
     )
 
@@ -80,10 +80,10 @@ def reset_password(session: SessionDep, body: NewPassword) -> Message:
     """
     Reset password
     """
-    email = verify_password_reset_token(token=body.token)
-    if not email:
+    user_id = verify_password_reset_token(token=body.token)
+    if not user_id:
         raise HTTPException(status_code=400, detail="Invalid token")
-    user = crud.get_user_by_email(session=session, email=email)
+    user = session.get(User, user_id)
     if not user:
         raise HTTPException(
             status_code=404,
