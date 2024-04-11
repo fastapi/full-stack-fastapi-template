507178339993c6c1b2e63fed65c695c9d0e8c7e9
Patrick Arminio <patrick.arminio@gmail.com>
2024-04-11 10:32:12 +0200

    ✨ use suspense for items page

--- frontend/src/routes/_layout/items.tsx
+++ frontend/src/routes/_layout/items.tsx
@@ -2,7 +2,7 @@ import {
   Container,
   Flex,
   Heading,
-  Spinner,
+  Skeleton,
   Table,
   TableContainer,
   Tbody,
@@ -11,82 +11,69 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react"
-import { useQuery } from "@tanstack/react-query"
+import { useSuspenseQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
 
+import { Suspense } from "react"
 import { ItemsService } from "../../client"
 import ActionsMenu from "../../components/Common/ActionsMenu"
 import Navbar from "../../components/Common/Navbar"
-import useCustomToast from "../../hooks/useCustomToast"
 
 export const Route = createFileRoute("/_layout/items")({
   component: Items,
 })
 
-function Items() {
-  const showToast = useCustomToast()
-  const {
-    data: items,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
+function ItemsTable() {
+  const { data: items } = useSuspenseQuery({
     queryKey: ["items"],
     queryFn: () => ItemsService.readItems({}),
   })
-
-  if (isError) {
-    const errDetail = (error as any).body?.detail
-    showToast("Something went wrong.", `${errDetail}`, "error")
-  }
-
   return (
-    <>
-      {isLoading ? (
-        // TODO: Add skeleton
-        <Flex justify="center" align="center" height="100vh" width="full">
-          <Spinner size="xl" color="ui.main" />
-        </Flex>
-      ) : (
-        items && (
-          <Container maxW="full">
-            <Heading
-              size="lg"
-              textAlign={{ base: "center", md: "left" }}
-              pt={12}
-            >
-              Items Management
-            </Heading>
-            <Navbar type={"Item"} />
-            <TableContainer>
-              <Table size={{ base: "sm", md: "md" }}>
-                <Thead>
-                  <Tr>
-                    <Th>ID</Th>
-                    <Th>Title</Th>
-                    <Th>Description</Th>
-                    <Th>Actions</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {items.data.map((item) => (
-                    <Tr key={item.id}>
-                      <Td>{item.id}</Td>
-                      <Td>{item.title}</Td>
-                      <Td color={!item.description ? "ui.dim" : "inherit"}>
-                        {item.description || "N/A"}
-                      </Td>
-                      <Td>
-                        <ActionsMenu type={"Item"} value={item} />
-                      </Td>
-                    </Tr>
-                  ))}
-                </Tbody>
-              </Table>
-            </TableContainer>
-          </Container>
-        )
-      )}
-    </>
+    <TableContainer>
+      <Table size={{ base: "sm", md: "md" }}>
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Title</Th>
+            <Th>Description</Th>
+            <Th>Actions</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {items.data.map((item) => (
+            <Tr key={item.id}>
+              <Td>{item.id}</Td>
+              <Td>{item.title}</Td>
+              <Td color={!item.description ? "ui.dim" : "inherit"}>
+                {item.description || "N/A"}
+              </Td>
+              <Td>
+                <ActionsMenu type={"Item"} value={item} />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+function Items() {
+  return (
+    <Container maxW="full">
+      <Heading size="lg" textAlign={{ base: "center", md: "left" }} pt={12}>
+        Items Management
+      </Heading>
+      <Suspense
+        fallback={
+          <Flex py={8} gap={4}>
+            <Skeleton height="40px" width={100} />
+          </Flex>
+        }
+      >
+        <Navbar type={"Item"} />
+        <ItemsTable />
+      </Suspense>
+    </Container>
   )
 }
