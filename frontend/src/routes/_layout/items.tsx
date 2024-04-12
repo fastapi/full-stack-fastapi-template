c48a81c8bd1e0765589accc55f824177ab42d80b
Patrick Arminio <patrick.arminio@gmail.com>
2024-04-12 12:28:47 +0200

    Use error boundary

--- frontend/src/routes/_layout/items.tsx
+++ frontend/src/routes/_layout/items.tsx
@@ -15,6 +15,7 @@ import { useSuspenseQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
 
 import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 import { ItemsService } from "../../client"
 import ActionsMenu from "../../components/Common/ActionsMenu"
 import Navbar from "../../components/Common/Navbar"
@@ -64,16 +65,19 @@ function Items() {
       <Heading size="lg" textAlign={{ base: "center", md: "left" }} pt={12}>
         Items Management
       </Heading>
-      <Suspense
-        fallback={
-          <Flex py={8} gap={4}>
-            <Skeleton height="40px" width={100} />
-          </Flex>
-        }
-      >
-        <Navbar type={"Item"} />
-        <ItemsTable />
-      </Suspense>
+
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Suspense
+          fallback={
+            <Flex py={8} gap={4}>
+              <Skeleton height="40px" width={100} />
+            </Flex>
+          }
+        >
+          <Navbar type={"Item"} />
+          <ItemsTable />
+        </Suspense>
+      </ErrorBoundary>
     </Container>
   )
 }
