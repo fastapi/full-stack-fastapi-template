33d74b2a80813083e5ec868a233a6c109109dd90
Muhammad Sameer Amin <sameeramin1997@gmail.com>
2024-05-20 12:06:14 +0500

    ♻️ Refactored code to use encryption algorithm name from settings for consistency

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -8,6 +8,7 @@ import emails  # type: ignore
 from jinja2 import Template
 from jose import JWTError, jwt
 
+from app.core import security
 from app.core.config import settings
 
 
@@ -103,14 +104,16 @@ def generate_password_reset_token(email: str) -> str:
     encoded_jwt = jwt.encode(
         {"exp": exp, "nbf": now, "sub": email},
         settings.SECRET_KEY,
-        algorithm="HS256",
+        algorithm=security.ALGORITHM,
     )
     return encoded_jwt
 
 
 def verify_password_reset_token(token: str) -> str | None:
     try:
-        decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        decoded_token = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
         return str(decoded_token["sub"])
     except JWTError:
         return None
