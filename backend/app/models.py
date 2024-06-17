d0defd5a92bfcd35ccea93873519816a2252a97e
Esteban Maya Cadavid <emayacadavid9@gmail.com>
2024-06-17 10:07:52 -0500

    🦺 Edit set max_length of 40 for password input data

--- backend/app/models.py
+++ backend/app/models.py
@@ -12,19 +12,19 @@ class UserBase(SQLModel):
 
 # Properties to receive via API on creation
 class UserCreate(UserBase):
-    password: str = Field(min_length=8, max_length=25)
+    password: str = Field(min_length=8, max_length=40)
 
 
 class UserRegister(SQLModel):
     email: EmailStr = Field(max_length=255)
-    password: str = Field(min_length=8, max_length=25)
+    password: str = Field(min_length=8, max_length=40)
     full_name: str | None = Field(default=None, max_length=255)
 
 
 # Properties to receive via API on update, all are optional
 class UserUpdate(UserBase):
     email: EmailStr | None = Field(default=None, max_length=255)  # type: ignore
-    password: str | None = Field(default=None, min_length=8, max_length=25)
+    password: str | None = Field(default=None, min_length=8, max_length=40)
 
 
 class UserUpdateMe(SQLModel):
@@ -33,8 +33,8 @@ class UserUpdateMe(SQLModel):
 
 
 class UpdatePassword(SQLModel):
-    current_password: str = Field(min_length=8, max_length=25)
-    new_password: str = Field(min_length=8, max_length=25)
+    current_password: str = Field(min_length=8, max_length=40)
+    new_password: str = Field(min_length=8, max_length=40)
 
 
 # Database model, database table inferred from class name
@@ -107,4 +107,4 @@ class TokenPayload(SQLModel):
 
 class NewPassword(SQLModel):
     token: str
-    new_password: str = Field(min_length=8, max_length=25)
+    new_password: str = Field(min_length=8, max_length=40)
