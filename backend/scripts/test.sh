51fddc1f67f157b2169c200cf7abaf221cc80be1
Jakub Nowakowski <jakub.n@logically.ai>
2024-06-28 18:10:36 +0200

    add optional --no-coverage flag

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -3,6 +3,21 @@
 set -e
 set -x
 
-coverage run --source=app -m pytest
-coverage report --show-missing
-coverage html --title "${@-coverage}"
+NO_COVERAGE=0
+
+# Check for the --no-coverage flag
+for arg in "$@"; do
+    if [ "$arg" == "--no-coverage" ]; then
+        NO_COVERAGE=1
+        shift
+        break
+    fi
+done
+
+if [ $NO_COVERAGE -eq 0 ]; then
+    coverage run --source=app -m pytest "$@"
+    coverage report --show-missing
+    coverage html --title "Coverage Report"
+else
+    pytest "$@"
+fi
