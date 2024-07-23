1f0d8c26b46e90c052ec0777641ee974e83f47b7
User <alejsdev@gmail.com>
2024-07-22 22:09:15 -0500

    Update config.ts

--- frontend/tests/config.ts
+++ frontend/tests/config.ts
@@ -1,7 +1,11 @@
 const { FIRST_SUPERUSER, FIRST_SUPERUSER_PASSWORD } = process.env;
 
-if (!FIRST_SUPERUSER || !FIRST_SUPERUSER_PASSWORD) {
-  throw new Error("Environment variables FIRST_SUPERUSER or FIRST_SUPERUSER_PASSWORD are undefined");
+if (!FIRST_SUPERUSER) {
+  throw new Error("Environment variable FIRST_SUPERUSER is undefined");
+}
+
+if (!FIRST_SUPERUSER_PASSWORD) {
+  throw new Error("Environment variable FIRST_SUPERUSER_PASSWORD is undefined");
 }
 
 export const firstSuperuser = FIRST_SUPERUSER as string;
