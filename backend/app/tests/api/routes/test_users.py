107ccaa939c7d27c696ed3d524b2bd4a2683d633
User <alejsdev@gmail.com>
2024-07-31 16:06:45 -0500

    Fix

--- backend/app/tests/api/routes/test_users.py
+++ backend/app/tests/api/routes/test_users.py
@@ -317,10 +317,7 @@ def test_register_user_already_exists_error(client: TestClient) -> None:
         json=data,
     )
     assert r.status_code == 400
-    assert (
-        r.json()["detail"]
-        == "The user with this email already exists in the system"
-    )
+    assert r.json()["detail"] == "The user with this email already exists in the system"
 
 
 def test_update_user(
