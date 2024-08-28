daab4e141aedaf89df24024afb072b89373babdd
User <alejsdev@gmail.com>
2024-08-28 21:43:55 +0200

    Add script to generate frontend client

---
+++ scripts/generate-client.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+PYTHONPATH=backend python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > openapi.json
+node frontend/modify-openapi-operationids.js
+mv openapi.json frontend/
+cd frontend || exit
+npm run generate-client
