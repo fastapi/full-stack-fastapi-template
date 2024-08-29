2b347cfe2ec25a38aaafc77a4c9e8eacbfeaac63
User <alejsdev@gmail.com>
2024-08-29 19:15:37 +0200

    Refactor

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -3,6 +3,6 @@
 PYTHONPATH=backend python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > openapi.json
 node frontend/modify-openapi-operationids.js
 mv openapi.json frontend/
-cd frontend || exit
+cd frontend
 npm run generate-client
 npx biome format --write ./src/client
