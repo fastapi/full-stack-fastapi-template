3d540b78ffe2b2d3dfbb774be95abbf22ae4cc6b
WardaMehboob <wardamehboob01@gmail.com>
2024-09-10 08:45:01 +0500

    fix(frontend/app): comment added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        {/* warda */}
+        {/* warda Mehboob */}
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
