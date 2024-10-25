cd040a1d4d4b0d072063914b5d7a749322a1cb85
Sebastián Ramírez <tiangolo@gmail.com>
2024-10-25 21:10:49 +0100

    ♻️ Use env vars for Playwright tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config'
 
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
