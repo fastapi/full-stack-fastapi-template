d6df2faedbf51f999f8eb15b709642f0627cd9ab
Patrick Arminio <patrick.arminio@gmail.com>
2024-11-26 17:35:00 +0000

    Update playwright

---
+++ frontend/tests/utils/privateApi.ts
@@ -0,0 +1,20 @@
+import { OpenAPI, PrivateService } from "../../src/client"
+
+OpenAPI.BASE = `${process.env.VITE_API_URL}`
+
+export const createUser = async ({
+  email,
+  password,
+}: {
+  email: string
+  password: string
+}) => {
+  return await PrivateService.createUser({
+    requestBody: {
+      email,
+      password,
+      is_verified: true,
+      full_name: "Test User",
+    },
+  })
+}
