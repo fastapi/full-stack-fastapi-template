ffb3d80d3f30859687724a3e6c3cda7231000b67
youneshenniwrites <youneshenniwrites@gmail.com>
2024-12-03 15:32:44 +0000

    lint: remove unused import

--- backend/app/api/routes/login.py
+++ backend/app/api/routes/login.py
@@ -9,7 +9,6 @@ from app import crud
 from app.api.deps import CurrentUser, SessionDep, get_current_active_superuser
 from app.core import security
 from app.core.config import settings
-from app.core.security import get_password_hash
 from app.models import Message, NewPassword, Token, UserPublic, UserUpdate
 from app.utils import (
     generate_password_reset_token,
