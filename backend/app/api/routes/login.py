83f6d292068328e08a3e4b55ef05b2d63359744b
Younes Henni <youneshenniwrites@gmail.com>
2024-12-03 14:38:54 +0000

    chore: rollback auto import changes

--- backend/app/api/routes/login.py
+++ backend/app/api/routes/login.py
@@ -1,18 +1,22 @@
 from datetime import timedelta
 from typing import Annotated, Any
 
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordRequestForm
+
 from app import crud
 from app.api.deps import CurrentUser, SessionDep, get_current_active_superuser
 from app.core import security
 from app.core.config import settings
 from app.core.security import get_password_hash
 from app.models import Message, NewPassword, Token, UserPublic
-from app.utils import (generate_password_reset_token,
-                       generate_reset_password_email, send_email,
-                       verify_password_reset_token)
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.responses import HTMLResponse
-from fastapi.security import OAuth2PasswordRequestForm
+from app.utils import (
+    generate_password_reset_token,
+    generate_reset_password_email,
+    send_email,
+    verify_password_reset_token,
+)
 
 router = APIRouter(tags=["login"])
 
