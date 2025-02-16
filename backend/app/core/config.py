71f4a1b7469f94d70108e4ea6276948a4e79b0a8
Joel Pérez Izquierdo <joelperez91@gmail.com>
2025-02-16 15:28:32 +0100

    ruff format config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -38,9 +38,9 @@ class Settings(BaseSettings):
     FRONTEND_HOST: str = "http://localhost:5173"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
-    BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = (
-        []
-    )
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str, BeforeValidator(parse_cors)
+    ] = []
 
     @computed_field  # type: ignore[prop-decorator]
     @property
