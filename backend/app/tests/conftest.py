2d24b5e81f7687b93f3e8a7db3e21e5f95cf3016
Robert Buels <rbuels@gmail.com>
2025-02-23 17:29:36 -0800

    fix patient and attachment test data sticking around after tests run

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -7,7 +7,7 @@ from sqlmodel import Session, delete
 from app.core.config import settings
 from app.core.db import engine, init_db
 from app.main import app
-from app.models import Item, User
+from app.models import Item, User, Patient, Attachment
 from app.tests.utils.user import authentication_token_from_email
 from app.tests.utils.utils import get_superuser_token_headers
 
@@ -20,6 +20,10 @@ def db() -> Generator[Session, None, None]:
         statement = delete(Item)
         session.execute(statement)
         statement = delete(User)
+        session.execute(statement)
+        statement = delete(Patient)
+        session.execute(statement)
+        statement = delete(Attachment)
         session.execute(statement)
         session.commit()
 
