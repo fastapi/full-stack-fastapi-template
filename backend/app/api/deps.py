c580a80ed1ea2a8a47be0891d8cd9b0147c1c86d
rony1philip <119314744+rony1philip@users.noreply.github.com>
2025-03-05 17:59:20 +0200

    dep patient

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -11,7 +11,7 @@ from sqlmodel import Session
 from app.core import security
 from app.core.config import settings
 from app.core.db import engine
-from app.models import TokenPayload, User
+from app.models import TokenPayload, User, Patient, Menu
 
 reusable_oauth2 = OAuth2PasswordBearer(
     tokenUrl=f"{settings.API_V1_STR}/login/access-token"
@@ -25,8 +25,6 @@ def get_db() -> Generator[Session, None, None]:
 
 SessionDep = Annotated[Session, Depends(get_db)]
 TokenDep = Annotated[str, Depends(reusable_oauth2)]
-
-
 def get_current_user(session: SessionDep, token: TokenDep) -> User:
     try:
         payload = jwt.decode(
@@ -55,3 +53,50 @@ def get_current_active_superuser(current_user: CurrentUser) -> User:
             status_code=403, detail="The user doesn't have enough privileges"
         )
     return current_user
+
+
+
+def get_current_patient(session: SessionDep, token: TokenDep) -> User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+    except (InvalidTokenError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = session.get(User, token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return user
+
+CurrentPatient = Annotated[Menu, Depends(get_current_menu)]
+
+def get_current_menu(session: SessionDep, token: TokenDep) -> Menu:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+    except (InvalidTokenError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    patient = session.get(Menu, token_data.sub)
+    if not patient:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not patient.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return patient
+
+
+CurrentMenu = Annotated[Menu, Depends(get_current_menu)]
+
+
+
+
