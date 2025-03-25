71c14a60104f7efc4826709ed8ce6893858e9931
mohamedachrefhacheni <achrefhacheni2003@gamil.com>
2025-03-25 03:00:35 +0100

    user_role_system_up

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
 import uuid
+from enum import Enum
 
 from pydantic import EmailStr
 from sqlmodel import Field, Relationship, SQLModel
@@ -39,16 +40,20 @@ class UpdatePassword(SQLModel):
     new_password: str = Field(min_length=8, max_length=40)
 
 
+class UserRole(str, Enum):
+    ADMIN: str = "admin"
+    USER:  str = "user"
+
 # Database model, database table inferred from class name
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
     items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
+    role: UserRole = Field(default=UserRole.USER)  
+   
 
-    class UserRole(str, Enum):
-        USER = "user"
-    ADMIN = "admin"
-    MODERATOR = "moderator"
+
+
 
 
 # Properties to return via API, id is always required
