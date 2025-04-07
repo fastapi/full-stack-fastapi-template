baf8766049cede851a16071346de76da62ccb59d
Marcelo Mizuno <marcelo@hotmail.co.jp>
2025-04-07 19:12:32 -0300

    Correção de relacionamento de classes

--- backend/app/models.py
+++ backend/app/models.py
@@ -47,6 +47,7 @@ class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
     items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
+    tickets: list["Ticket"] = Relationship(back_populates="user", cascade_delete=True)
 
 
 # Properties to return via API, id is always required
