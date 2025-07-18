8fe60c315f9678c22d4a75fbb2c3cd91cc7f8f2a
myrkytyn <maksmykytyn@gmail.com>
2025-07-18 22:45:45 +0300

    remove unused import

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -11,7 +11,6 @@ from pydantic import (
     computed_field,
     model_validator,
 )
-from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing_extensions import Self
 
