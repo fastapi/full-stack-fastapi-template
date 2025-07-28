fea3bdad373998f11654012718818c2ab00d6a70
Monica Smith <monica.smith463@gmail.com>
2025-07-28 14:34:30 -0700

    skip using alembic for now

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -16,10 +16,10 @@ def init_db(session: Session) -> None:
     # Tables should be created with Alembic migrations
     # But if you don't want to use migrations, create
     # the tables un-commenting the next lines
-    # from sqlmodel import SQLModel
+    from sqlmodel import SQLModel
 
     # This works because the models are already imported and registered from app.models
-    # SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)
 
     user = session.exec(
         select(User).where(User.email == settings.FIRST_SUPERUSER)
