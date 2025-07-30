5bd87ff755c8520b22afff39d1e8fafb52adfc43
Monica Smith <monica.smith463@gmail.com>
2025-07-30 10:06:30 -0700

    remove alembic from backend/app/core/db.py for now

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,4 +1,4 @@
-from sqlmodel import Session, create_engine, select
+from sqlmodel import SQLModel, Session, create_engine, select
 
 from app import crud
 from app.core.config import settings
@@ -19,7 +19,7 @@ def init_db(session: Session) -> None:
     # from sqlmodel import SQLModel
 
     # This works because the models are already imported and registered from app.models
-    # SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)
 
     user = session.exec(
         select(User).where(User.email == settings.FIRST_SUPERUSER)
