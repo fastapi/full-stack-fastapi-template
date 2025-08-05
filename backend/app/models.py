5c6cfbbb0673586b675fced0393ec32d6d3b11ec
Monica Smith <monica.smith463@gmail.com>
2025-08-05 13:29:57 -0700

    make Item model inactive

--- backend/app/models.py
+++ backend/app/models.py
@@ -120,28 +120,22 @@ class NewPassword(SQLModel):
     token: str
     new_password: str = Field(min_length=8, max_length=40)
 
-#TODO: Delete Item later.
+#TODO: Delete Item later. Keep for now so frontend build passes.
 
 class ItemBase(SQLModel):
-    title: str = Field(min_length=1, max_length=255)
-    description: str | None = Field(default=None, max_length=255)
+    pass
 
 class ItemCreate(ItemBase):
     pass
 
 class ItemUpdate(ItemBase):
-    title: str | None = Field(default=None, min_length=1, max_length=255)  # type: ignore
+    pass
 
 class Item(ItemBase, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="items")
+    pass
 
 class ItemPublic(ItemBase):
-    id: uuid.UUID
-    owner_id: uuid.UUID
-    filename: str
-    content_type: str | None = None
-    size: int | None = None
+    pass
+
+class ItemsPublic(SQLModel):
+    pass
