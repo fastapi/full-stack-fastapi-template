e9483f36e6c96900d4a4fd06268d5c3f7e89aed5
vicaya <vicaya@users.noreply.github.com>
2025-08-20 05:55:03 +0000

    Fix broken unit tests #1780

--- backend/app/tests/scripts/test_backend_pre_start.py
+++ backend/app/tests/scripts/test_backend_pre_start.py
@@ -1,7 +1,8 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, Mock, patch
 
 from sqlmodel import select
 
+import app.backend_pre_start
 from app.backend_pre_start import init, logger
 
 
@@ -9,11 +10,14 @@ def test_init_successful_connection() -> None:
     engine_mock = MagicMock()
 
     session_mock = MagicMock()
-    exec_mock = MagicMock(return_value=True)
-    session_mock.configure_mock(**{"exec.return_value": exec_mock})
+    session_mock.__enter__.return_value = session_mock
+    session_mock.exec = Mock()
+
+    select1 = select(1)
 
     with (
-        patch("sqlmodel.Session", return_value=session_mock),
+        patch("app.backend_pre_start.Session", return_value=session_mock),
+        patch("app.backend_pre_start.select", return_value=select1),
         patch.object(logger, "info"),
         patch.object(logger, "error"),
         patch.object(logger, "warn"),
@@ -28,6 +32,4 @@ def test_init_successful_connection() -> None:
             connection_successful
         ), "The database connection should be successful and not raise an exception."
 
-        assert session_mock.exec.called_once_with(
-            select(1)
-        ), "The session should execute a select statement once."
+        session_mock.exec.assert_called_once_with(select1)
