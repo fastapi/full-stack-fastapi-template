2fcaec2910dc462160f4952b28ba7585e2a3a45f
goldendevuz <goldendevuz@gmail.com>
2025-09-07 19:38:54 +0500

    2025.09.07/19:38

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -27,7 +27,7 @@ def parse_cors(v: Any) -> list[str] | str:
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         # Use top level .env file (one level above ./backend/)
-        env_file="../.env",
+        env_file="../../.env",
         env_ignore_empty=True,
         extra="ignore",
     )
