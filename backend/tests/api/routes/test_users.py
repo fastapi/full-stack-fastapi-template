0f6907f8c9e920b516fc6cbb4806d83de2a5e0a5
Yurii Motov <yurii.motov.monte@gmail.com>
2025-09-08 16:22:20 +0200

    Move `backend/app/tests` to `backend/tests`

--- backend/tests/api/routes/test_users.py
+++ backend/tests/api/routes/test_users.py
@@ -8,7 +8,7 @@ from app import crud
 from app.core.config import settings
 from app.core.security import verify_password
 from app.models import User, UserCreate
-from app.tests.utils.utils import random_email, random_lower_string
+from tests.utils.utils import random_email, random_lower_string
 
 
 def test_get_users_superuser_me(
