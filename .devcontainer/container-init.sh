7a4b61e2419d0c97fedb74ac9624f3cfa8f26829
George Mazzeo <mazzeogeorge@gmail.com>
2025-09-19 22:13:25 +0000

    devcontainer

---
+++ .devcontainer/container-init.sh
@@ -0,0 +1,11 @@
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH="$HOME/.cargo/bin:$PATH"
+
+
+# Claude Code install
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="/usr/local/share/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
+npm install -g @anthropic-ai/claude-code
