77992a5d383e630b06dc63317586333cc7c6520c
Roland Takacs <rolland0208@gmail.com>
2025-09-20 19:06:44 +0200

    🐛 Fix `parse_cors` function to be consistent for both empty string and empty list (#1672)

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -17,7 +17,7 @@ from typing_extensions import Self
 
 def parse_cors(v: Any) -> list[str] | str:
     if isinstance(v, str) and not v.startswith("["):
-        return [i.strip() for i in v.split(",")]
+        return [i.strip() for i in v.split(",") if i.strip()]
     elif isinstance(v, list | str):
         return v
     raise ValueError(v)
