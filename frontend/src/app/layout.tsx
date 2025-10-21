e0c75e004175f8348f5d584f8c54031e8437e7d4
Monica Smith <monica.smith463@gmail.com>
2025-10-21 15:20:44 -0700

    signUpForm use-form hook set up

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,18 @@
+'use client';
+
 import { Outfit } from 'next/font/google';
 import './globals.css';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const outfit = Outfit({
   subsets: ["latin"],
 });
+
+const queryClient = new QueryClient();
+
 
 export default function RootLayout({
   children,
@@ -15,10 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} dark:bg-gray-900`}>
-        <ThemeProvider>
-          <SidebarProvider>{children}</SidebarProvider>
-        </ThemeProvider>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <SidebarProvider>{children}</SidebarProvider>
+          </ThemeProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
--- frontend/src/components/form/input/InputField.tsx
+++ frontend/src/components/form/input/InputField.tsx
@@ -8,8 +8,8 @@ interface InputProps {
   defaultValue?: string | number;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
-  min?: string;
-  max?: string;
+  min?: string | number;
+  max?: string | number;
   step?: number;
   disabled?: boolean;
   success?: boolean;
