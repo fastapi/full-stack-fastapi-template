556c18614892e8ac696a421dd7e3268b5f96b7a5
Monica Smith <monica.smith463@gmail.com>
2025-10-21 12:36:46 -0700

    remove tailadmin

--- frontend/src/app/(full-width-pages)/(auth)/signup/page.tsx
+++ frontend/src/app/(full-width-pages)/(auth)/signup/page.tsx
@@ -2,8 +2,8 @@ import SignUpForm from "@/components/auth/SignUpForm";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next.js SignUp Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js SignUp Page TailAdmin Dashboard Template",
+  title: "Next.js SignUp Page",
+  description: "This is Next.js SignUp Page",
   // other metadata
 };
 
--- frontend/src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
+++ frontend/src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js Error 404 | TailAdmin - Next.js Dashboard Template",
+  title: "Next.js Error 404",
   description:
-    "This is Next.js Error 404 page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+    "This is Next.js Error 404 page",
 };
 
 export default function Error404() {
@@ -46,9 +46,6 @@ export default function Error404() {
         </Link>
       </div>
       {/* <!-- Footer --> */}
-      <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
-        &copy; {new Date().getFullYear()} - TailAdmin
-      </p>
     </div>
   );
 }
