46c988835ac4fcfee448ee88649d96f6bd7b9d9d
Monica Smith <monica.smith463@gmail.com>
2025-10-22 13:39:45 -0700

    signup form successfully posts to backend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,13 @@ import { Outfit } from 'next/font/google';
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { OpenAPI } from "@/client/core/OpenAPI";
+
+OpenAPI.TOKEN = async () => {
+  return localStorage.getItem("access_token") || ""
+}
+
+OpenAPI.BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 const queryClient = new QueryClient();
 
