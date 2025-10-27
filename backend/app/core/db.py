48621da101a63f4b71ee32487c6f50cbf5458cb9
JesusGabrielCastro <jesusgabrielcv@ufps.edu.co>
2025-10-27 00:07:30 -0500

    fix role superuser

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -2,7 +2,7 @@ from sqlmodel import Session, create_engine, select
 
 from app import crud
 from app.core.config import settings
-from app.models import User, UserCreate
+from app.models import User, UserCreate, UserRole
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
@@ -29,5 +29,6 @@ def init_db(session: Session) -> None:
             email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
+            role=UserRole.ADMINISTRADOR
         )
         user = crud.create_user(session=session, user_create=user_in)
