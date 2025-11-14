e2802f9f14cc173922b36f7810a78892d7e54716
Kjell Reidar Jøssang <95139113+Tinnitussen@users.noreply.github.com>
2025-11-14 18:33:05 +0100

    remove template

--- backend/scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-mypy app
-ruff check app
-ruff format app --check
--- backend/scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-coverage run -m pytest tests/
-coverage report
-coverage html --title "${@-coverage}"
