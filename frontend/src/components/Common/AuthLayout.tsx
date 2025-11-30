d25c25ba95811b2dab7c10973b539bbab01cb567
Alejandra <alejsdev@gmail.com>
2025-11-30 21:19:59 +0100

    ✨ Add AuthLayout component

---
+++ frontend/src/components/Common/AuthLayout.tsx
@@ -0,0 +1,24 @@
+import Logo from "/assets/images/fastapi-logo.svg"
+
+interface AuthLayoutProps {
+  children: React.ReactNode
+}
+
+export function AuthLayout({ children }: AuthLayoutProps) {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="bg-muted relative hidden lg:flex lg:items-center lg:justify-center">
+        <img
+          src={Logo}
+          alt="Logo"
+          className="h-16 w-auto dark:brightness-[0.8] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">{children}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
