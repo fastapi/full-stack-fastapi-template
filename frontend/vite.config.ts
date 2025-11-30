0972b3ebfa17bdb173f180587a21c2da95c6b095
Alejandra <alejsdev@gmail.com>
2025-11-30 15:33:45 +0100

    🔧 Add Tailwind, update dependencies and config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path"
 import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +17,6 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react(),
+    tailwindcss(),
   ],
 })
