cbfb714a22409a10a570438ba1a66e418879d41c
Alejandra <alejsdev@gmail.com>
2025-12-01 15:02:37 +0100

    ♻️ Refactor error handling in utils

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,12 +1,19 @@
+import { AxiosError } from "axios"
 import type { ApiError } from "./client"
-import useCustomToast from "./hooks/useCustomToast"
 
-export const handleError = (err: ApiError) => {
-  const { showErrorToast } = useCustomToast()
-  const errDetail = (err.body as any)?.detail
-  let errorMessage = errDetail || "Something went wrong."
-  if (Array.isArray(errDetail) && errDetail.length > 0) {
-    errorMessage = errDetail[0].msg
+function extractErrorMessage(err: ApiError): string {
+  if (err instanceof AxiosError) {
+    return err.message
   }
-  showErrorToast(errorMessage)
+
+  const errDetail = (err.body as any)?.detail
+  if (Array.isArray(errDetail) && errDetail.length > 0) {
+    return errDetail[0].msg
+  }
+  return errDetail || "Something went wrong."
+}
+
+export const handleError = function (this: (msg: string) => void, err: ApiError) {
+  const errorMessage = extractErrorMessage(err)
+  this(errorMessage)
 }
