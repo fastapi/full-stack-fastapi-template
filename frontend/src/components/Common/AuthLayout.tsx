d7e348acb449d8d565742ee25c6a59246a265822
Alejandra <alejsdev@gmail.com>
2025-12-02 16:35:47 +0100

    ♻️ Refactor Common components

--- frontend/src/components/Common/AuthLayout.tsx
+++ frontend/src/components/Common/AuthLayout.tsx
@@ -1,4 +1,5 @@
-import Logo from "/assets/images/fastapi-logo.svg"
+import { Appearance } from "@/components/Common/Appearance"
+import { Logo } from "@/components/Common/Logo"
 import { Footer } from "./Footer"
 
 interface AuthLayoutProps {
@@ -8,14 +9,13 @@ interface AuthLayoutProps {
 export function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="bg-muted relative hidden lg:flex lg:items-center lg:justify-center">
-        <img
-          src={Logo}
-          alt="Logo"
-          className="h-16 w-auto dark:brightness-[0.8] dark:grayscale"
-        />
+      <div className="bg-muted dark:bg-zinc-900 relative hidden lg:flex lg:items-center lg:justify-center">
+        <Logo variant="full" className="h-16" asLink={false} />
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-end">
+          <Appearance />
+        </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">{children}</div>
         </div>
