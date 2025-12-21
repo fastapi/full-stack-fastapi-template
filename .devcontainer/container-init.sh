b396c73de3889b0d21b35d286502e7cd10aca527
George Mazzeo <mazzeogeorge@gmail.com>
2025-12-21 00:47:28 +0000

    feat: Add Stripe CLI installation and update container initialization script

--- .devcontainer/container-init.sh
+++ .devcontainer/container-init.sh
@@ -9,11 +9,18 @@ export NVM_DIR="/usr/local/share/nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 npm install -g @anthropic-ai/claude-code
+claude mcp add context7 -- npx -y @upstash/context7-mcp
 
 
-# install github cli 
+# install github cli
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update
 apt install gh -y
+
+# stripe cli
+curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public | gpg --dearmor | sudo tee /usr/share/keyrings/stripe.gpg
+echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | sudo tee -a /etc/apt/sources.list.d/stripe.list
+sudo apt update
+sudo apt install stripe -y
