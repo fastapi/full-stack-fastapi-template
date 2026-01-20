e3a9cbe62d1e93cb16475a168bce64b065fc1853
Alejandra <alejsdev@gmail.com>
2026-01-20 21:41:50 +0100

    Fix conflicts

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -7,5 +7,5 @@ cd backend
 python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
 cd ..
 mv openapi.json frontend/
-cd frontend
-npm run generate-client
+npm run -w frontend generate-client
+npm run lint
