fdfc7459d2821c07be70f0f205ed51fa535efc44
Sebastián Ramírez <tiangolo@gmail.com>
2026-01-22 19:34:14 +0100

    🔨 Tweak generate client script

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -7,5 +7,5 @@ cd backend
 uv run python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../openapi.json
 cd ..
 mv openapi.json frontend/
-bun run -w frontend generate-client
+bun run --filter frontend generate-client
 bun run lint
