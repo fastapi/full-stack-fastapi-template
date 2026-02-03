b5030de51e977ad52b1e1b97e35e486954fd68fe
Motov Yurii <109919500+YuriiMotov@users.noreply.github.com>
2026-02-03 21:28:50 +0300

    👷 Run mypy by pre-commit (#2169)

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -10,6 +10,7 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
+assert config.config_file_name is not None
 fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
