acb8c096ca4f77aeb58f5cf2aef0ff61e3eb3637
lucas-hudsn <lucashudsn@gmail.com>
2026-02-04 14:59:49 +1100

    i

--- backend/app/alembic/versions/a1b2c3d4e5f6_add_movie_watchlist_rating_models.py
+++ backend/app/alembic/versions/a1b2c3d4e5f6_add_movie_watchlist_rating_models.py
@@ -1,7 +1,7 @@
 """Add movie watchlist rating models
 
 Revision ID: a1b2c3d4e5f6
-Revises: 1a31ce608336
+Revises: fe56fa70289e
 Create Date: 2026-02-04
 
 """
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = "a1b2c3d4e5f6"
-down_revision = "1a31ce608336"
+down_revision = "fe56fa70289e"
 branch_labels = None
 depends_on = None
 
