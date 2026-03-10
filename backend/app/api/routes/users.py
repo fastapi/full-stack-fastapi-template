61d8a05def5dd1ebd1e6f65e73a55b3f9204d696
svlandeg <sofie.vanlandeghem@gmail.com>
2026-03-10 14:41:28 +0100

    convert Item/User to ItemPublic/UserPublic

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -47,7 +47,8 @@ def read_users(session: SessionDep, skip: int = 0, limit: int = 100) -> Any:
     )
     users = session.exec(statement).all()
 
-    return UsersPublic(data=users, count=count)
+    users_public = [UserPublic.model_validate(user) for user in users]
+    return UsersPublic(data=users_public, count=count)
 
 
 @router.post(
