97d6713b873e92e72138ec8c2bf179fe0fccd518
Raphael Tannous <raphaeltannous@proton.me>
2026-05-25 11:36:34 +0300

    Add function to create a random item for a user

--- backend/tests/utils/item.py
+++ backend/tests/utils/item.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlmodel import Session
 
 from app import crud
@@ -9,6 +11,14 @@ from tests.utils.utils import random_lower_string
 def create_random_item(db: Session) -> Item:
     user = create_random_user(db)
     owner_id = user.id
+    assert owner_id is not None
+    title = random_lower_string()
+    description = random_lower_string()
+    item_in = ItemCreate(title=title, description=description)
+    return crud.create_item(session=db, item_in=item_in, owner_id=owner_id)
+
+
+def create_random_item_for_user(db: Session, owner_id: uuid.UUID) -> Item:
     assert owner_id is not None
     title = random_lower_string()
     description = random_lower_string()
