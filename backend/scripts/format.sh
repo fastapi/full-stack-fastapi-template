807bfb06381760bef28554a6a688ab0c62a39d2e
Sungho <hurgj123kr@gmail.com>
2026-05-28 09:03:21 +0900

    폴더정리

--- backend/scripts/format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-set -x
-
-ruff check app scripts --fix
-ruff format app scripts
--- backend/scripts/lint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-mypy app
-ty check app
-ruff check app
-ruff format app --check
