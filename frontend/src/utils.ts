5fd1470db08ad984386d00ec72e4b82a2c39a8bd
Yurii Motov <yurii.motov.monte@gmail.com>
2026-07-23 20:34:04 +0200

    Migrate client config and error handling to client-axios

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,22 +1,20 @@
 import { AxiosError } from "axios"
-import type { ApiError } from "./client"
 
-function extractErrorMessage(err: ApiError): string {
+function extractErrorMessage(err: Error): string {
   if (err instanceof AxiosError) {
+    const errDetail = (err.response?.data as any)?.detail
+    if (Array.isArray(errDetail) && errDetail.length > 0) {
+      return errDetail[0].msg
+    }
+    if (typeof errDetail === "string") {
+      return errDetail
+    }
     return err.message
   }
-
-  const errDetail = (err.body as any)?.detail
-  if (Array.isArray(errDetail) && errDetail.length > 0) {
-    return errDetail[0].msg
-  }
-  return errDetail || "Something went wrong."
+  return "Something went wrong."
 }
 
-export const handleError = function (
-  this: (msg: string) => void,
-  err: ApiError,
-) {
+export const handleError = function (this: (msg: string) => void, err: Error) {
   const errorMessage = extractErrorMessage(err)
   this(errorMessage)
 }
