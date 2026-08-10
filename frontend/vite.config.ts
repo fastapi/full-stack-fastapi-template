e021d7a3a5d0872aadda19490a8c0a2cc71a21e7
Yurii Motov <yurii.motov.monte@gmail.com>
2026-08-10 14:21:54 +0200

    Address `__dirname` unsupported by `configLoader: 'native'` warning

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
   plugins: [
