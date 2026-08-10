505af274a5ec33b6cd10c1f2ccbd6fa7a2805ae8
Sebastián Ramírez <tiangolo@gmail.com>
2026-08-10 18:13:49 +0200

    ♻️ Simplify optional environment configuration

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -34,7 +34,7 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
-    ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    ENVIRONMENT: Literal["local"] | None = None
 
     BACKEND_CORS_ORIGINS: Annotated[
         list[AnyUrl] | str, BeforeValidator(parse_cors)
